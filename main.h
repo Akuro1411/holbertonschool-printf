2fc64ec09c95c4881f4a5d8851b916987ee87373
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-27 15:51:18 +0000

    putchar file added

--- main.h
+++ main.h
@@ -13,10 +13,7 @@
  */
 
 int _printf(const char *format, ...);
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 va_list ptr;
 void pd();
 void pi();
---
+++ putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _putchar - Print the parameter
+ * @c: Character
+ * Return: Always 0
+ * -------------------------------------------------------------
+ * Source File: _putchar.c - Print the character
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
