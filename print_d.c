30bc9f297c72d358a58b1be665bd89ce40d39b3e
Akuro1411 <nahidisayev147@gmail.com>
2024-03-29 10:09:09 +0000

    all new codes are added to all files and styled for betty style

--- print_d.c
+++ print_d.c
@@ -1,20 +1,58 @@
 #include "main.h"
-
-void print_number(long n)
+/**
+ * print_num - function printf number
+ * @len: - shows the  length of number
+ * @arg: - va_list element from main
+ * Return: Number of printed characters(excluding the null) or numbers.
+ */
+int print_num(int len, va_list arg)
 {
+	int64_t a;
 
-    // If number is smaller than 0, put a - sign
-    // // and change number to positive
-	if (n < 0) 
+	a = va_arg(arg, int);
+	if (a < 0)
 	{
-		putchar('-');
-		n = -n;
+		len++;
+	}
+	print(a);
+	len += count(a);
+	return (len);
+}
+
+/**
+ * count - counts the digits of number
+ * @n: - the given number from _printf
+ * Return: Number of printed characters(excluding the null) or numbers.
+ */
+
+
+int count(int n)
+{
+	int l = 1;
+
+	if (n == 0)
+		return (0);
+	else
+		return  (l + count(n / 10));
+}
+
+/**
+ * print - prints number
+ * @a: - the given number from printf
+ * Return: Number of printed characters(excluding the null) or numbers.
+ */
+
+int print(int64_t a)
+{
+	if (a < 0)
+	{
+		_putchar('-');
+		a = -a;
 	}
 
-    // Remove the last digit and recur
-	if (n/10)
-		print_number(n/10);
+	if (a / 10)
+		print(a / 10);
 
-    // Print the last digit
-	putchar(n%10 + '0');
+	_putchar((a % 10) + '0');
+	return (0);
 }
--- print_str.c
+++ print_str.c
@@ -1,20 +1,24 @@
 #include "main.h"
 /**
  * ps - Prints string
+ * @len: - the number of elements in string
+ * @arg: - va_list element which belong to main file
  * Return: number of characters printed
  */
-void ps(void)
+
+int ps(int len, va_list arg)
 {
 	int j = 0;
 	char *str;
 
-	str = va_arg(ptr, char *);
+	str = va_arg(arg, char *);
 	if (str == NULL)
-		str = "(null)";
+	str = "(null)";
 	while (str[j] != '\0')
 	{
 		_putchar(str[j]);
 		j++;
-		l++;
+		len++;
 	}
+	return (len);
 }
