3a73008e961bc546b31d88021113ad6363543788
Akuro1411 <nahidisayev147@gmail.com>
2024-03-29 09:42:38 +0000

    printf is changed again and new codes are included in header

--- main.h
+++ main.h
@@ -4,14 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <limits.h>
+#include <stdint.h>
+#include <inttypes.h>
+ 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int l;
-va_list ptr;
-void pd(void);
-void pi(void);
-void print(int a);
-void pc(void);
-void ps(void);
+int _putchar(char c); 
+int print_num(int len, va_list arg);
+int print(int64_t a);
+int count(int n);
+int pc(va_list arg);
+int ps(int len, va_list arg);
+
 #endif
